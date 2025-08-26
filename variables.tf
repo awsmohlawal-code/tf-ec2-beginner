4f0da26e97791b300713f608fefea16f54ed047b
awsmohlawal-code <aws.moh.lawal@gmail.com>
2025-08-26 04:10:06 -0500

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "AWS key pair name for SSH access"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block for SSH access"
+  type        = string
+}
