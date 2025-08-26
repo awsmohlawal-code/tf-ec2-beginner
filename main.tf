f658320c104643fcd2bf96714f449eba102ee007
awsmohlawal-code <aws.moh.lawal@gmail.com>
2025-08-26 04:20:20 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,1 +1,42 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+
+  tags = {
+    Name = "MyFirstEC2"
+  }
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i ${var.key_pair_name}.pem ec2-user@${aws_instance.my_ec2.public_ip}"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
 
+output "ssh_command" {
+  description = "SSH command to connect to the EC2 instance"
+  value       = "ssh -i ${var.key_pair_name}.pem ec2-user@${aws_instance.my_ec2.public_ip}"
+}
